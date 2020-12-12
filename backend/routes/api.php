fc9c1a3c32f862a3cc94f30cea746a21e67ca855
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-12-12 02:29:41 -0500

    Rutas y Controladores

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,6 +20,7 @@ use Illuminate\Http\Request;
 Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@authenticate');
 //Route::get('festivals', 'FestivalController@index');
+Route::get('/places', 'PlaceController@index');
 
 //Rutas protegidas o privadas
 Route::group(['middleware' => ['jwt.verify']], function () {
@@ -28,7 +29,15 @@ Route::group(['middleware' => ['jwt.verify']], function () {
 
     //Bandas
         //GET, POST, PUT , DELETE
-    //Recursos
-
+    //FESTIVAL
+    //GET
+    //GET by ID
+        Route::get('/places/{place}', 'PlaceController@show');
+    //POST
+        Route::post('/places', 'PlaceController@store');
+    //PUT
+        Route::put('/places/{place}', 'PlaceController@update');
+    //DELETE
+        Route::delete('/places/{place}', 'PlaceController@delete');
     //Etc...
 });
