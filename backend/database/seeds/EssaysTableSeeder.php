2e4cb8281f6322c3350c39418c90a44eb1dde03f
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-12-11 20:55:08 -0500

    Seeder Essays

---
+++ backend/database/seeds/EssaysTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Essay;
+
+class EssaysTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Essay::truncate(); //truncate es un método mágica
+
+        $faker = \Faker\Factory::create(); //utilizaremos el método crear de Faker
+
+        for ($i=0; $i < 7; $i++) {
+            //Llamamos un método estático:
+            Essay::create([
+                //Atributos $fillable
+                'dateEssay' => $faker -> dateTime, //Genra LoremIpsum para name, de tipo dateTime
+                'name' => $faker -> name,
+                'place' => $faker -> name,
+
+            ]);
+
+        }
+    }
+}
