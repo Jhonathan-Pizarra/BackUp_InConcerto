087d563e301f1e7685bfaf0cccb4ffee8dcd9462
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-12-11 22:22:22 -0500

    Seeder Concierto

---
+++ backend/database/seeds/ConcertsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Concert;
+
+class ConcertsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Concert::truncate(); //truncate es un método mágica
+
+        $faker = \Faker\Factory::create(); //utilizaremos el método crear de Faker
+
+        for ($i=0; $i < 7; $i++) {
+            //Llamamos un método estático:
+            Concert::create([
+                //Atributos $fillable
+                'dateConcert' => $faker -> dateTime, //Genra LoremIpsum para name, de tipo dateTime
+                'name' => $faker -> name,
+                'duration' => $faker ->time('H:i:s'),
+                'free' => $faker -> boolean,
+                'insitu' => $faker -> boolean,
+
+            ]);
+
+        }
+    }
+}
