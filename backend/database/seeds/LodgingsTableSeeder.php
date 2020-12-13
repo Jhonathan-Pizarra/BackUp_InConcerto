b5a048b16ff8b8a7fa76a77f421650e4174b3892
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-12-13 02:41:02 -0500

    Seeder Hospedaje!

---
+++ backend/database/seeds/LodgingsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Lodging;
+
+class LodgingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Lodging::truncate(); //truncate es un método mágica
+
+        $faker = \Faker\Factory::create(); //utilizaremos el método crear de Faker
+
+        for ($i=0; $i < 7; $i++) {
+            //Llamamos un método estático:
+            Lodging::create([
+                //Atributos $fillable
+                'name' => $faker -> name,
+                'type' => $faker -> word,
+                'description' => $faker ->word,
+                'observation' => $faker -> sentence,
+                'checkIn' => $faker -> date('Y-m-d'),
+                'checkOut' => $faker -> date('Y-m-d'),
+
+            ]);
+
+        }
+    }
+}
