5222a51c143187e11bc4de7da30b30ab8dc55671
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-12-13 03:33:12 -0500

    Seeder Transporte

---
+++ backend/database/seeds/TransportsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Transport;
+
+class TransportsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Transport::truncate(); //truncate es un método mágica
+
+        $faker = \Faker\Factory::create(); //utilizaremos el método crear de Faker
+
+        for ($i=0; $i < 7; $i++) {
+            //Llamamos un método estático:
+            Transport::create([
+                //Atributos $fillable
+                'type' => $faker -> name,
+                'capacity' => $faker ->randomDigitNotNull,
+                'instruments_capacity' => $faker -> randomFloat(2, 0, 1),
+                'disponibility' => $faker -> boolean,
+                'licence_plate' => $faker -> word,
+
+            ]);
+
+        }
+    }
+}
