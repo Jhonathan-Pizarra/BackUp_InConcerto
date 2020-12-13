532ea4c832838b4ea4cec3369ebcb062ffcf1557
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-12-13 18:17:48 -0500

    Seeder Alimentacion

---
+++ backend/database/seeds/FeedingsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Feeding;
+
+class FeedingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Feeding::truncate(); //truncate es un método mágica
+
+        $faker = \Faker\Factory::create(); //utilizaremos el método crear de Faker
+
+        for ($i=0; $i < 7; $i++) {
+            //Llamamos un método estático:
+            Feeding::create([
+                //Atributos $fillable
+                'date' => $faker -> dateTime, //Genra LoremIpsum para name, de tipo dateTime
+                'food' => $faker -> name,
+                'observation' => $faker ->text,
+                'quantityLunchs' => $faker -> randomDigitNotNull,
+
+            ]);
+
+        }
+    }
+}
