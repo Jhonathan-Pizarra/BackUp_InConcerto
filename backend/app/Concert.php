488a666aff07eaeaedfc1885b908a48e23da6721
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-12-30 15:01:28 -0500

    Eloquent Concierto-RecursoConcierto

--- backend/app/Concert.php
+++ backend/app/Concert.php
@@ -8,4 +8,11 @@ class Concert extends Model
 {
     protected $fillable = ['dateConcert', 'name', 'duration', 'free', 'insitu'];
 
+
+    public function concerts()
+    {
+        return $this->hasMany('App\ConcertResource'); //Eloquent determina la FK automáticamente
+    }
 }
+
+
