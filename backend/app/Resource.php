f224c93cc293c71b4c7f993c0d0dfed3f186d69c
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-12-30 16:04:15 -0500

    Eloquent ConcertResource-Resource

--- backend/app/Resource.php
+++ backend/app/Resource.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Resource extends Model
 {
     protected $fillable = ['name', 'quantity', 'description'];
+
+    //Tiene:
+    public function concert_resources()
+    {
+        return $this->hasMany('App\ConcertResource'); //Eloquent determina la FK automáticamente
+    }
+
 }
