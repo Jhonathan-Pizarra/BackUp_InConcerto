8120e2a2498b719545648718ed807c15c66f19b1
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-12-30 22:32:05 -0500

    Eloquent Transport-Calendar

--- backend/app/Transport.php
+++ backend/app/Transport.php
@@ -8,4 +8,10 @@ class Transport extends Model
 {
     protected $fillable = ['type', 'capacity', 'instruments_capacity', 'disponibility', 'licence_plate'];
 
+    //Pertenece a:
+    public function calendar()
+    {
+        return $this->belongsTo('App\Calendar');
+    }
+
 }
