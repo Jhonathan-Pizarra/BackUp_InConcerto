eb60c5c8280cfbbd9ba6ca106116758997cd7829
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-01-11 17:00:02 -0500

    Eloquent Concierto-LugarConcierto

--- backend/app/Place.php
+++ backend/app/Place.php
@@ -8,4 +8,10 @@ class Place extends Model
 {
     protected $fillable = ['name','address','permit', 'aforo', 'description'];
 
+    //Relación Concierto-LugarConcierto
+    public function concerts()
+    {
+        return $this->hasMany('App\Concert'); //Eloquent determina la FK automáticamente
+    }
+
 }
