054f3f5506180e8129ff9b523c92c9f52a6207f3
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-01-11 18:23:34 -0500

    Eloquent_Ensayo-Festival

--- backend/app/Essay.php
+++ backend/app/Essay.php
@@ -8,4 +8,10 @@ class Essay extends Model
 {
     protected $fillable = ['dateEssay', 'name', 'place'];
 
+    //Relacion Ensayo-Festival
+    public function festival()
+    {
+        return $this->belongsTo('App\Festival');
+    }
+
 }
