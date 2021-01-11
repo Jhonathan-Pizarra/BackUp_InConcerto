168e1b519233cd94cae46b7b5b5c4ad2d63a061e
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-01-11 17:56:08 -0500

    Eloquent_Concierto-Festival

--- backend/app/Festival.php
+++ backend/app/Festival.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Festival extends Model
 {
     protected $fillable = ['name', 'description'];
+
+    //Relacion Festival-Concierto
+    public function concerts()
+    {
+        return $this->hasMany('App\Concert'); //Eloquent determina la FK automáticamente
+    }
+
 }
