05190fed589a892cccfd40aa99be02bcc2349dc6
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-01-11 16:50:21 -0500

    Relacion People-Hospedaje

---
+++ backend/database/migrations/2021_01_11_163845_create_coordination_lodgings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoordinationLodgingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coordination_lodgings', function (Blueprint $table) {
+            $table->unsignedBigInteger('lodging_id');
+            $table->foreign('lodgings_id')->references('id')->on('lodgings')->onDelete('restrict');
+            $table->unsignedBigInteger('artist_id');
+            $table->foreign('artist_id')->references('id')->on('artists')->onDelete('restrict');
+            $table->unsignedBigInteger('admins_id');
+            $table->foreign('admins_id')->references('id')->on('users')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('coordination_lodgings');
+        Schema::enableForeignKeyConstraints();
+    }
+}
