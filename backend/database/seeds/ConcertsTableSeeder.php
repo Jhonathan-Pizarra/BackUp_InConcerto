853f4201efdbdde9294c22c34ec80c615b15e61b
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-01-15 18:46:28 -0500

    Seeder_Concierto-Recursos

--- backend/database/seeds/ConcertsTableSeeder.php
+++ backend/database/seeds/ConcertsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Concert;
+use App\Resource;
 
 class ConcertsTableSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class ConcertsTableSeeder extends Seeder
 
         for ($i=0; $i < 7; $i++) {
             //Llamamos un método estático:
-            Concert::create([
+            $concert = Concert::create([
                 //Atributos $fillable
                 'dateConcert' => $faker -> dateTime, //Genra LoremIpsum para name, de tipo dateTime
                 'name' => $faker -> name,
@@ -29,7 +30,20 @@ class ConcertsTableSeeder extends Seeder
                 'place_id' => $faker -> numberBetween(1,5),
 
             ]);
-
+            //Un concierto puede tener 1 recurso, el recurso X,o  2 recursos (X e Y)... o todos los 7
+            $concert->resources()->saveMany(
+                $faker->randomElements(
+                    array(
+                        Resource::find(1),
+                        Resource::find(2),
+                        Resource::find(3),
+                        Resource::find(4),
+                        Resource::find(5),
+                        Resource::find(6),
+                        Resource::find(7)
+                    ), $faker->numberBetween(1, 7), false)
+            );
         }
+
     }
 }
