d3174410f0b70a159697af9c84fcba1c4b7cddc3
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-01-19 17:32:12 -0500

    JWT

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -67,7 +67,7 @@ class UserController extends Controller
         } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
             return response()->json(['message' => 'token_absent'], $e->getStatusCode());
         }
-        return response()->json(compact($user), 200);
+        return response()->json(compact('user'), 200);
     }
 
 }
