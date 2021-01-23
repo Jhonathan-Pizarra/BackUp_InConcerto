b394d2cd814b2d5577ae89ee55c408181b89f471
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-01-22 23:06:09 -0500

    Validation Artist

--- backend/app/Http/Controllers/ArtistController.php
+++ backend/app/Http/Controllers/ArtistController.php
@@ -51,6 +51,23 @@ class ArtistController extends Controller
     }
 
     public function update(Request $request, Artist $artist){
+
+        $request->validate([
+            'ciOrPassport' => 'required|string|unique:artists|max:15',
+            'artisticOrGroupName' => 'required|string|unique:artists|max:255',
+            'name' => 'required|string|max:255',
+            'lastName' => 'required|string|max:255',
+            'nationality' => 'required|string|max:255',
+            'mail' => 'required|string|unique:artists|email|max:255',
+            'phone' => 'required|string|max:10',
+            'passage' => 'required|boolean',
+            'instruments' => 'required|string',
+            'emergencyPhone' => 'required|string|max:25',
+            'emergencyMail' => 'required|string|email|max:255',
+            'foodGroup' => 'required|string|max:255',
+            'observation' => 'required|string',
+        ], self::$messages);
+
         $artist -> update($request->all());
         return response() -> json($artist, 200); //codigo 200 correspodnde a modificacion exitosa
     }
