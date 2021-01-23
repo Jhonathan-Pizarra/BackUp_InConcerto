98487b6a078017266c754f6388b2ab5ba92015e9
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-01-22 23:52:00 -0500

    Validation Hospedaje

--- backend/app/Http/Controllers/LodgingController.php
+++ backend/app/Http/Controllers/LodgingController.php
@@ -9,6 +9,11 @@ use App\Http\Resources\LodgingCollection;
 
 class LodgingController extends Controller
 {
+    private static $messages = [
+        'required' => 'El campo :attribute es obligatorio.',
+        'date' => ':attribute debe tener formato YYYY-MM-DD.',
+    ];
+
     //Vamos a hacer controladores, tareas que debe realizar
     public function index(){
         return new LodgingCollection(Lodging::paginate());
@@ -19,11 +24,31 @@ class LodgingController extends Controller
     }
 
     public function store(Request $request){
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'type' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'observation' => 'required|string',
+            'checkIn' => 'required|date',
+            'checkOut' => 'required|date'
+        ], self::$messages);
+
         $lodging = Lodging::create($request ->all());
         return response() -> json($lodging, 201); //codigo 201 correspodnde a create
     }
 
     public function update(Request $request, Lodging $lodging){
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'type' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'observation' => 'required|string',
+            'checkIn' => 'required|date',
+            'checkOut' => 'required|date'
+        ], self::$messages);
+
         $lodging -> update($request->all());
         return response() -> json($lodging, 200); //codigo 200 correspodnde a modificacion exitosa
     }
