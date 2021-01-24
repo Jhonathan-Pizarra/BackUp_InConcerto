632e4f6a76ffd25eb2a1a6070d28da4781d973a2
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-01-23 21:56:02 -0500

    Validation FeedingPlace

--- backend/app/Http/Controllers/FeedingPlaceController.php
+++ backend/app/Http/Controllers/FeedingPlaceController.php
@@ -9,6 +9,12 @@ use App\Http\Resources\FeedingPlaceCollection;
 
 class FeedingPlaceController extends Controller
 {
+    private static $messages = [
+        'required' => 'El campo :attribute es obligatorio.',
+        'integer' => 'El campo :attribute no es un número entero.',
+        'boolean' => 'El campo :attribute debe ser true o false'
+    ];
+
     //Vamos a hacer controladores, tareas que debe realizar
     public function index(){
         return new FeedingPlaceCollection(FeedingPlace::paginate());
@@ -19,11 +25,29 @@ class FeedingPlaceController extends Controller
     }
 
     public function store(Request $request){
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|unique:feeding_places|max:255',
+            'permit' => 'required|boolean',
+            'aforo' => 'required|integer',
+
+        ], self::$messages);
+
         $feeding_place = FeedingPlace::create($request ->all());
         return response() -> json($feeding_place, 201); //codigo 201 correspodnde a create
     }
 
     public function update(Request $request, FeedingPlace $feeding_place){
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|unique:articles|max:255',
+            'permit' => 'required|boolean',
+            'aforo' => 'required|integer',
+
+        ], self::$messages);
+
         $feeding_place -> update($request->all());
         return response() -> json($feeding_place, 200); //codigo 200 correspodnde a modificacion exitosa
     }
