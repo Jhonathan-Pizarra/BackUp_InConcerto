ae93dc1fc9182905b03fbf8a28ce334e30f6e942
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-02-02 20:07:15 -0500

    Correcion

--- backend/app/Http/Controllers/ArtistCalendarController.php
+++ backend/app/Http/Controllers/ArtistCalendarController.php
@@ -33,10 +33,19 @@ class ArtistCalendarController extends Controller
     public function store(Request $request, Calendar $calendar){
 
         $request->validate([
-            'dateEssay' => 'required|date',
+            'ciOrPassport' => 'required|string|unique:artists|max:15',
+            'artisticOrGroupName' => 'required|string|unique:artists|max:255',
             'name' => 'required|string|max:255',
-            'place' => 'required|string|max:255',
-            'festival_id' => 'required|exists:festivals,id', //No disponible hasta que se fusione con la rama de relacion
+            'lastName' => 'required|string|max:255',
+            'nationality' => 'required|string|max:255',
+            'mail' => 'required|string|unique:artists|email|max:255',
+            'phone' => 'required|string|max:10',
+            'passage' => 'required|boolean',
+            'instruments' => 'required|string',
+            'emergencyPhone' => 'required|string|max:25',
+            'emergencyMail' => 'required|string|email|max:255',
+            'foodGroup' => 'required|string|max:255',
+            'observation' => 'required|string',
         ], self::$messages);
 
         $artist = $calendar->artists()->save(new Artist($request->all()));
@@ -46,10 +55,19 @@ class ArtistCalendarController extends Controller
     public function update(Request $request, Calendar $calendar, Artist $artist){
 
         $request->validate([
-            'dateEssay' => 'required|date',
+            'ciOrPassport' => 'required|string|unique:artists|max:15',
+            'artisticOrGroupName' => 'required|string|unique:artists|max:255',
             'name' => 'required|string|max:255',
-            'place' => 'required|string|max:255',
-            'festival_id' => 'required|exists:festivals,id', //No disponible hasta que se fusione con la rama de relacion
+            'lastName' => 'required|string|max:255',
+            'nationality' => 'required|string|max:255',
+            'mail' => 'required|string|unique:artists|email|max:255',
+            'phone' => 'required|string|max:10',
+            'passage' => 'required|boolean',
+            'instruments' => 'required|string',
+            'emergencyPhone' => 'required|string|max:25',
+            'emergencyMail' => 'required|string|email|max:255',
+            'foodGroup' => 'required|string|max:255',
+            'observation' => 'required|string',
         ], self::$messages);
 
         $artist = $calendar->artists()->where('id', $artist->id)->firstOrFail();
