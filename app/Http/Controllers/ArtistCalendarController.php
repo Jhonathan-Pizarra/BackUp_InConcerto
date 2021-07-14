19b4ee59e1bc2e9751f9b697a9b517268a2abd1a
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-07-14 13:28:19 -0500

    Correccion Artist-Calendar

--- app/Http/Controllers/ArtistCalendarController.php
+++ app/Http/Controllers/ArtistCalendarController.php
@@ -22,16 +22,39 @@ class ArtistCalendarController extends Controller
     ];
 
     public function index(Calendar $calendar){
-        return response()->json(ArtistCalRes::collection($calendar->artists),200);
+        //return response()->json(ArtistCalRes::collection($calendar->artists),200);
+        return response()->json(new ArtistCollection($calendar->artists),200);  //no data + metadata
     }
 
     public function show(Calendar $calendar, Artist $artist){
-        $artist = $calendar->artists()->where('artist_id', $artist->id)->firstOrFail();
-        return response()->json($artist, 200);
+        $calendarArtist = $calendar->artists()->where('artist_id', $artist->id)->firstOrFail();
+        return response()->json($calendarArtist, 200);
     }
 
-    public function store(Request $request, Calendar $calendar){
+    public function store(Request $request){
 
+        $request->validate([
+            'artist_id' => 'exists:artists,id',
+            'calendar_id' => 'exists:calendars,id',
+        ], self::$messages);
+
+        $calendar = Calendar::findOrFail($request->calendar_id);
+        $calendar->artists()->attach($request->artist_id);
+        return response()->json($calendar->artists, 200);
+
+        /*
+        //$artistID = Artist::findOrFail($artist->id); //Busco en los artistas el del id ¿Cual...?
+        $calendarID = Calendar::findOrFail($calendar->id); //Busco en calendarios el que tenga el id...cual?
+
+        $artist->calendars()->attach($calendarID);
+        $artist->save();
+        return response()->json($artist, 201);
+        /*
+
+        //$artist = $calendar->artists()->save(new Artist($request->all()));
+        //return response()->json($artist, 201);
+
+        /*
         $request->validate([
             'ciOrPassport' => 'required|string|unique:artists|max:15',
             'artisticOrGroupName' => 'required|string|unique:artists|max:255',
@@ -40,7 +63,7 @@ class ArtistCalendarController extends Controller
             'nationality' => 'required|string|max:255',
             'mail' => 'required|string|unique:artists|email|max:255',
             'phone' => 'required|string|max:10',
-            'passage' => 'required|boolean',
+            'passage' => 'required',
             'instruments' => 'required|string',
             'emergencyPhone' => 'required|string|max:25',
             'emergencyMail' => 'required|string|email|max:255',
@@ -49,25 +72,25 @@ class ArtistCalendarController extends Controller
         ], self::$messages);
 
         $artist = $calendar->artists()->save(new Artist($request->all()));
-        return response()->json($artist, 201);
+        return response()->json($artist, 201); */
     }
 
     public function update(Request $request, Calendar $calendar, Artist $artist){
 
         $request->validate([
-            'ciOrPassport' => 'required|string|unique:artists|max:15',
-            'artisticOrGroupName' => 'required|string|unique:artists|max:255',
-            'name' => 'required|string|max:255',
-            'lastName' => 'required|string|max:255',
-            'nationality' => 'required|string|max:255',
-            'mail' => 'required|string|unique:artists|email|max:255',
-            'phone' => 'required|string|max:10',
-            'passage' => 'required|boolean',
-            'instruments' => 'required|string',
-            'emergencyPhone' => 'required|string|max:25',
-            'emergencyMail' => 'required|string|email|max:255',
-            'foodGroup' => 'required|string|max:255',
-            'observation' => 'required|string',
+            'ciOrPassport' => 'string|unique:artists|max:15',
+            'artisticOrGroupName' => 'string|unique:artists|max:255',
+            'name' => 'string|max:255',
+            'lastName' => 'string|max:255',
+            'nationality' => 'string|max:255',
+            'mail' => 'string|unique:artists|email|max:255',
+            'phone' => 'string|max:10',
+            'passage' => 'required',
+            'instruments' => 'string',
+            'emergencyPhone' => 'string|max:25',
+            'emergencyMail' => 'string|email|max:255',
+            'foodGroup' => 'string|max:255',
+            'observation' => 'string',
         ], self::$messages);
 
         $artist = $calendar->artists()->where('id', $artist->id)->firstOrFail();
@@ -76,9 +99,16 @@ class ArtistCalendarController extends Controller
     }
 
     public function delete(Request $request,  Calendar $calendar, Artist $artist){
-    $artist = $calendar->artists()->where('id', $artist->id)->firstOrFail();
-    $artist -> delete();
-    return response() -> json(null, 404); //codigo 204 correspodnde a not found
-}
+
+        $calendar = Calendar::findOrFail($calendar->id);
+        $calendar->artists()->detach($artist->id);
+        return response()->json(null, 200);
+
+        /*
+        $artist = $calendar->artists()->where('id', $artist->id)->firstOrFail();
+        $artist -> delete();
+        return response() -> json(null, 404); //codigo 204 correspodnde a not found
+        */
+    }
 
 }
