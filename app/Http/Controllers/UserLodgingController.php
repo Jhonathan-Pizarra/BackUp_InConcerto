aaac66018eada8e900f11bb44dce0f8d61ffd068
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2021-07-20 15:45:50 -0500

    R-Lodgings-User

--- app/Http/Controllers/UserLodgingController.php
+++ app/Http/Controllers/UserLodgingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Lodging;
 use App\Http\Resources\User as UserLodRes;
+use App\Http\Resources\UserCollection;
 
 use Illuminate\Http\Request;
 
@@ -15,26 +16,28 @@ class UserLodgingController extends Controller
     ];
 
     public function index(Lodging $lodging){
-        return response()->json(UserLodRes::collection($lodging->users),200);
+        return response()->json(new UserCollection($lodging->users),200);
+        //return response()->json(UserLodRes::collection($lodging->users),200);
     }
 
     public function show(Lodging $lodging, User $user){
-        $user = $lodging->users()->where('id', $user->id)->firstOrFail();
-        return response()->json($user, 200);
+        $lodgingUser = $lodging->users()->where('id', $user->id)->firstOrFail();
+        return response()->json($lodgingUser, 200);
     }
 
     public function store(Request $request, Lodging $lodging){
 
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|unique:users|email|max:255',
-            'password' => 'required|string',
+            'user_id' => 'exists:users,id',
+            'lodging_id' => 'exists:lodgings,id',
         ], self::$messages);
 
-        $user = $lodging->users()->save(new User($request->all()));
-        return response()->json($user, 201);//codigo 201 correspodnde a create
+        $lodging = Lodging::findOrFail($request->lodging_id);
+        $lodging->users()->attach($request->user_id);
+        return response()->json($lodging->users, 201);//codigo 201 correspodnde a create
     }
 
+    /*
     public function update(Request $request, Lodging $lodging, User $user){
 
         $request->validate([
@@ -47,11 +50,13 @@ class UserLodgingController extends Controller
         $user -> update($request->all());
         return response() -> json($user, 200); //codigo 200 correspodnde a modificacion exitosa
     }
+    */
 
     public function delete(Request $request, Lodging $lodging, User $user){
-        $user = $lodging->concerts()->where('id', $user->id)->firstOrFail();
-        $user -> delete();
-        return response() -> json(null, 404); //codigo 204 correspodnde a not found
+
+        $lodging = Lodging::findOrFail($lodging->id);
+        $lodging->users()->detach($user->id);
+        return response()->json(null, 404);
     }
 
 
